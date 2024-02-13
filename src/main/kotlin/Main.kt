4804aab44010a17d15d738b9b000a7d069e9f183
Salakhov.R.R <robert_salakhov@bk.ru>
2024-02-13 21:23:48 +0300

    ex6

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,9 +1,15 @@
 fun main() {
     val productsArray = arrayOf("Макарошки", "Гречка", "Сало", "Пшено", "Рис", "Кусок металла")
     val productsCostArray = arrayOf(14, 69, 240, 31, 46, 10000)
+    var costBag = 0
 
-    val inputProduct = readln()
-    val costProduct = productsCostArray[productsArray.indexOf(inputProduct)]
-
-    println("Товар \"$inputProduct\" стоит $costProduct кредитов")
+    while (true) {
+        val inputProduct = readln()
+        if (inputProduct == "Продуктовая корзина пустая") {
+            break
+        } else {
+            costBag += productsCostArray[productsArray.indexOf(inputProduct)]
+        }
+    }
+    println("Стоимость всех товаров в продуктовой корзине: $costBag кредитов")
 }
