cc6e0eb915ef6b2472b926ff0a7bc2433debeaa6
Salakhov.R.R <robert_salakhov@bk.ru>
2024-02-14 08:49:57 +0300

    ex8

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,4 +1,49 @@
 fun main() {
+    val analyze = "Анализ"
+    val pay = "Оплата"
+    val cancel = "Отмена"
+
+    printMainMenu()
+    val selectedItemMenu = readItemMenu()
+    when (selectedItemMenu) {
+        analyze -> analyzeOrder()
+        pay -> payOrder()
+        cancel -> cancelOrder()
+    }
+}
+
+fun printMainMenu() {
+    println(
+        "1. Посчитать стоимость продуктовой корзины\n" +
+                "2. Оплатить покупку\n" +
+                "3. Отменить покупку\n"
+    )
+}
+
+fun readItemMenu(): String {
+    val userChoice = readln()
+    var retunrnedString = ""
+
+    when (userChoice) {
+        "Анализ" -> {
+            println("Идет анализ продуктовой корзины\n")
+            retunrnedString = "Анализ"
+        }
+
+        "Оплата" -> {
+            println("Происходит процесс оплаты\n")
+            retunrnedString = "Оплата"
+        }
+
+        "Отмена" -> {
+            println("Идет отмена покупки\n")
+            retunrnedString = "Отмена"
+        }
+    }
+    return retunrnedString
+}
+
+fun analyzeOrder() {
     val productsArray = arrayOf("Макарошки", "Гречка", "Сало", "Пшено", "Рис", "Кусок металла")
     val productsCostArray = arrayOf(14, 69, 240, 31, 46, 10000)
     var costBag = 0
@@ -13,11 +58,19 @@ fun main() {
         if (indexInputProduct > -1) {
             costBag += productsCostArray[indexInputProduct]
         } else {
-            println("Товар \"$inputProduct\" не найден в базе данных")
+            println("Товар \"$inputProduct\" не найден в базе данных\n")
             break
         }
     }
     if (indexInputProduct > -1) {
         println("Стоимость всех товаров в продуктовой корзине: $costBag кредитов")
     }
+}
+
+fun payOrder() {
+    println("Оплата прошла успешно")
+}
+
+fun cancelOrder() {
+    println("Покупка отменена")
 }
